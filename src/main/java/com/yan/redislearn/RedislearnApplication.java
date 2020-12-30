faeedd363ffeb61d6091f324de9c086596fcf6d9
Stray-Alex <1005218593@qq.com>
2020-12-30 14:11:05 +0800

    Initial commit

---
+++ src/main/java/com/yan/redislearn/RedislearnApplication.java
@@ -0,0 +1,13 @@
+package com.yan.redislearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedislearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedislearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yan/redislearn/RedislearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.redislearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedislearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
